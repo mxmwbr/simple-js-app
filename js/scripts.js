15e971607dbad88387393c39e498a23295476853
mxmwbr <137923989+mxmwbr@users.noreply.github.com>
2025-01-28 17:03:34 +0100

    variables added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', Height: 2, type: ['seed', 'grass']},
+    {name: 'Charmander', Height: 2, type: ['Lizard', 'fire']},
+    {name: 'Squirtle', Height: 1, type: ['Tiny Turtle', 'water']}
+    ];
