e88daa43cd8f2cb0699f7e717eac972e5eb37d75
mxmwbr <137923989+mxmwbr@users.noreply.github.com>
2025-01-30 17:02:07 +0100

    for loop created

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
+// Array-list of Pokemons
 let pokemonList = [
     {name: 'Bulbasaur', Height: 2, type: ['seed', 'grass']},
     {name: 'Charmander', Height: 2, type: ['Lizard', 'fire']},
     {name: 'Squirtle', Height: 1, type: ['Tiny Turtle', 'water']}
     ];
+    
+for (let i = 0; i < pokemonList.length; i++) {
+        document.write("<p>" + `${pokemonList[i].name}: ${pokemonList[i].Height}` + "</p>")
+    }
+for (let i = 0; i < pokemonList.length; i++) {
+        if (pokemonList[i].Height > 2) {
+            document.write("<p>" + `${pokemonList[i].name} Wow, that's big!` + "</p>")
+        }
+    }
